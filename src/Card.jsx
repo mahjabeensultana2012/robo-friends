e1c4989e9d838872e041f67921911441160f1580
Mahjabeen Sultana <mahjabeen.sultana2012@gmail.com>
2020-01-13 12:38:40 -0600

    build card

---
+++ src/Card.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Card = () => (
+  <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+    <img src="https://robohash.org/testtest?200x200" alt="robots" />
+    <div>
+      <h2>Nishi</h2>
+      <p>nishi@gmail.com</p>
+    </div>
+  </div>
+);
+
+export default Card;
