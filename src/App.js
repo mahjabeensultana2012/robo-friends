2e03aef5daa67522ed411599502f7b473b377d20
Mahjabeen Sultana <mahjabeen.sultana2012@gmail.com>
2020-01-14 23:46:40 -0600

    implement searchbox

--- src/App.js
+++ src/App.js
@@ -13,15 +13,20 @@ class App extends React.Component {
   }
 
   handleChange = event => {
-    console.log(event.target.value);
+    this.setState({ searchfield: event.target.value });
   };
 
   render() {
+    const filteredRobots = this.state.robots.filter(robot => {
+      return robot.name
+        .toLowerCase()
+        .includes(this.state.searchfield.toLowerCase());
+    });
     return (
       <div className="tc">
         <h1>RoboFriends</h1>
         <SearchBox handleChange={this.handleChange} />
-        <CardList robots={this.state.robots} />
+        <CardList robots={filteredRobots} />
       </div>
     );
   }
