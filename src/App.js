163ddd7589f912f048529b4684960ca91f985e78
Mahjabeen Sultana <mahjabeen.sultana2012@gmail.com>
2020-01-14 23:19:44 -0600

    transform App component from function to class

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import CardList from './CardList';
+import SearchBox from './SearchBox';
+import { robots } from './robots';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: robots,
+      searchfield: '',
+    };
+  }
+  render() {
+    return (
+      <div className="tc">
+        <h1>RoboFriends</h1>
+        <SearchBox />
+        <CardList robots={robots} />
+      </div>
+    );
+  }
+}
+
+export default App;
